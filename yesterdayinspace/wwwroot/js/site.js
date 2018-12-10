a450763bfdb267769da2031688cf4cda7f3575d5
Logan Burrell <loganeaburrell@gmail.com>
2018-12-10 15:17:40 +1300

    Initial bio implimentation

--- yesterdayinspace/wwwroot/js/site.js
+++ yesterdayinspace/wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+// Shamelessly taken from librarium-sqlite
+$(document).ready(function() {
+	// We can basically use this code on any circle-section as
+	// the attributes are mostly the same. Checks are done in showBio()
+	$(".yis-circlesect").on("click","li", function(event){
+		event.preventDefault();
+		name = $(this).attr('data-name');
+		char = $(this).attr('data-charname');
+		img = $(this).attr('data-fullimg');
+		bio = $(this).attr('data-bio');
+		showBio(name, char, img, bio);
+	});
+});
+
+function showBio(name, char, img, bio) {
+	var n,c,i,b,fn;
+	n = name;
+	c = char;
+	i = img;
+	b = bio;
+
+	fn = "<b>" + n + "</b>";
+
+	if(c) { fn = fn + "<br />" + c; }
+
+	$('#YIS-BioFullImg').attr('src', '/images/profiles/' + i);
+	$('#YIS-BioName').html(fn);
+	$('#YIS-BioInfo').html(b);
+	$('#YIS-BioPane').fadeIn();
+}
